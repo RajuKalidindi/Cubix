c3400886ba72b5c34b707974636f2aaba2efa2c5
RajuKalidindi <krkraju997@gmail.com>
2018-09-02 20:48:31 +0530

    First commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    bool GameEnd = false;
+    public float restartDelay = 1f;
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel ()
+    {
+        completeLevelUI.SetActive(true); 
+    }
+
+    public void GameOver()
+    {
+        if (GameEnd == false)
+        {
+            GameEnd = true;
+            Debug.Log("Game Over");
+            Invoke("Restart", restartDelay);
+        }      
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Scripts/LevelComplete.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelComplete : MonoBehaviour {
+
+public void loadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    } 
+}
